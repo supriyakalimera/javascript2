2ebfd4469fcbc10eeda18e7c6fcd213b424a6be8
supriyakalimera <98305320+supriyakalimera@users.noreply.github.com>
2023-09-21 19:21:45 +0530

    Add files via upload

---
+++ day 08/javascriptdemo.js
@@ -0,0 +1,8 @@
+function sumoftwo(){
+    var n1 = parseInt(document.getElementById("id").value);
+    var n2=parseInt(document.getElementById("id").value);
+    var sum=n1+n2;
+    document.getElementById("res").innerHTML=sum;
+ 
+}
+sumoftwo();
